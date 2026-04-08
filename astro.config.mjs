21b6b61cae88fb1ed0291ce42b51333834e5ba1e
MemeBuddy <memebuddy@example.com>
2026-04-08 11:14:16 +0800

    downgrade_astro4_to_astro3

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,4 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://memebuddy.cc',
   integrations: [],
-  vite: {
-    ssr: {
-      external: ['svgo']
-    }
-  }
 });
